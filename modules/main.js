bb5839d444730e9f1b56e52e9f571b868f8380f0
AsrarK <asrarkaiser@gmail.com>
2023-01-16 22:04:04 -0500

    Lab1

--- modules/main.js
+++ modules/main.js
@@ -1,2 +1,6 @@
 import {halfOf} from "./lib.js";
+import {multiply} from "./lib.js";
+import doSomething from "./doSomething.js";
 console.log(halfOf(84));
+console.log(multiply(21, 2));
+doSomething();
